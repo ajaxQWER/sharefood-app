3a62e101d7713cdeab78a423a06593588a3368c4
liuqin <1184153305@qq.com>
2017-11-11 09:59:20 +0800

    营业分析主页

--- src/components/BusinessAnalysis.vue
+++ src/components/BusinessAnalysis.vue
@@ -10,30 +10,25 @@
       <div class="column-wrap">
         <div class="column flex">
           <div class="column-item flex-1">
-            <img src="" alt="">
-            <div class="column-name">新客户</div>
+            <img src="../assets/images/new-customer.png" alt="">
           </div>
 
           <div class="column-item flex-1">
-            <img src="" alt="">
-            <div class="column-name">订单量</div>
+            <img src="../assets/images/order.png" alt="">
           </div>
 
           <div class="column-item flex-1">
-            <img src="" alt="">
-            <div class="column-name">营业额</div>
+            <img src="../assets/images/business-volume.png" alt="">
           </div>
 
         </div>
         <div class="column flex">
           <div class="column-item flex-1">
-            <img src="" alt="">
-            <div class="column-name">销售量</div>
+            <img src="../assets/images/sales.png" alt="">
           </div>
 
           <div class="column-item flex-1">
-            <img src="" alt="">
-            <div class="column-name">销售量排行</div>
+            <img src="../assets/images/sales-rank.png" alt="">
           </div>
           <div class="column-item flex-1 column-null"></div>
 
@@ -68,19 +63,14 @@
     width: 29.73vw;
     height: 30.13vw;
     background-color: #fff;
+    border-radius: 2px;
   }
   .column-item:nth-child(3n+2){
     margin: 0 2.67vw;
   }
   .column-item img{
-    width: 13.73vw;
-    height: 13.73vw;
-    border-radius: 100%;
-    margin: 5.06vw auto;
-  }
-  .column-name{
-    font-size: 3.73vw;
-    text-align: center;
+    width: 29.73vw;
+    height: 30.13vw;
   }
   .column-null{
     background-color: #f2f2f2;
