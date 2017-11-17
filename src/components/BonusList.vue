d9367f4d8731b4667dbbe09e0cf73cc8201c56b0
xuejiangjun <449590828@qq.com>
2017-11-17 16:56:42 +0800

    新增红包模块

--- src/components/BonusList.vue
+++ src/components/BonusList.vue
@@ -12,21 +12,38 @@
 				<img src="../assets/images/empty-img.png" alt="">
 			</div>
 			<ul class="bonus-lists" v-else>
-				<li>
-					<div class="bonus-info"></div>
-					<div class="bonus-del-btn">删除</div>
+				<li v-for="(item,index) in bonusList" :key="index">
+					<div class="bonus-info">
+						<div class="bonus-value">
+							<div class="bonus-face">￥<span>12</span></div>
+							<div class="bonus-condition">满30元可用</div>
+						</div>
+						<div class="bonus-detail">
+							<div class="bonus-name">红包名称红包名称红包名称红包名称</div>
+							<div class="bonus-deadline">2017-11-18到期</div>
+						</div>
+					</div>
+					<div class="bonus-del-btn" @click="deleteBonus()">删除</div>
 				</li>
 			</ul>
 		</div>
 	</div>
 </template>
 <script>
+	import {getBonusLists,deleteBonusById} from '@/api/api'
 	export default {
 		name: 'bonusList',
 		data: function(){
 			return {
-				isEmpty: false
+				isEmpty: false,
+				bonusList: []
 			}
+		},
+		created: function(){
+
+		},
+		methods: {
+
 		}
 	}
 </script>
@@ -68,5 +85,69 @@
 		background-color: #fff;
 		margin: 2.66vw;
 		box-sizing: border-box;
+	}
+	.bonus-info{
+		width: 72.66vw;
+		float: left;
+		overflow: hidden;
+		zoom: 1;
+	}
+	.bonus-value{
+		width: 28vw;
+		height: 10.66vw;
+		padding: 6vw 0;
+		text-align: center;
+		float: left;
+		position: relative;
+	}
+	.bonus-value:before{
+		content: '';
+		display: inline-block;
+		position: absolute;
+		left: 0;
+		top: 0;
+		width: 8.26vw;
+		height: 8.66vw;
+		background: url(../assets/images/bonus-before.png) no-repeat center center;
+		background-size: contain;
+
+	}
+	.bonus-face{
+		font-size: 3.73vw;
+		color: #f41827;
+	}
+	.bonus-face span{
+		font-size: 5.33vw;
+		font-weight: bold;
+	}
+	.bonus-detail{
+		width: 44.66vw;
+		height: 10.66vw;
+		float: left;
+		padding: 6vw 0;
+	}
+	.bonus-name{
+		font-size: 4.26vw;
+		width: 100%;
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+	}
+	.bonus-condition{
+		color: #aaa;
+	}
+	.bonus-condition,
+	.bonus-deadline{
+		font-size: 3.2vw;
+	}
+	.bonus-del-btn{
+		width: 21.6vw;
+		height: 10.66vw;
+		padding: 6vw 0;
+		line-height: 10.66vw;
+		text-align: center;
+		float: right;
+		background-color: #ff3a2f;
+		color: #fff;
 	}
 </style>
