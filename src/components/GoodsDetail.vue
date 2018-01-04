9f74bc25237a293f24787c37aaaeda6b0013bfb1
xuejiangjun <449590828@qq.com>
2018-01-04 11:07:37 +0800

    修改图片上传问题

--- src/components/GoodsDetail.vue
+++ src/components/GoodsDetail.vue
@@ -410,17 +410,17 @@ export default {
 
             //处理异常,将ascii码小于0的转换为大于0
             var ab = new ArrayBuffer(bytes.length);
-            // var ia = new Uint8Array(ab);
-            // for (var i = 0; i < bytes.length; i++) {
-            //     ia[i] = bytes.charCodeAt(i);
-            // }
+            var ia = [];
+            for (var i = 0; i < bytes.length; i++) {
+                ia.push(bytes.charCodeAt(i));
+            }
 
-            var obj = new Blob([ab], { type: 'image/jpeg' })
+            var obj = new Blob([new Uint8Array(ia)], { type: 'image/jpeg' })
 
             var fd = new FormData();
 
             //formdata对象第3个参数为文件名，不传则默认使用文件名，这里是blob对象所以需要加一个后缀
-            fd.append('file', obj, '*.jpg');
+            fd.append('file', obj, Date.now() + '.jpg');
 
             fd.path = '/goods'
             this.$indicator.open();
